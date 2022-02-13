48edce041cb304d336817a8b65bf36da42242e6d
Lacy Sharratt <lacy.sharratt@gmail.com>
2022-02-13 17:04:40 -0500

    upload

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,8 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
 // Get data from cities.js
 // let cityData = cities;
@@ -27,49 +27,12 @@ let sanFranAirport =
             "coordinates":[-122.375,37.61899948120117]}}
 ]};
 
-// Grabbing our GeoJSON data.
-// L.geoJSON(sanFranAirport).addTo(map);
-
-// Grabbing our GeoJSON data.
-    // L.geoJSON(sanFranAirport, {
-    //     // We turn each feature into a marker on the map.
-    //     pointToLayer: function(feature, latlng) {
-    //       console.log(feature);
-    //       return L.marker(latlng)
-    //       .bindPopup("<h2>" + feature.properties.city + "</h2>")
-    //     }
-
 L.geoJson(sanFranAirport,{
     onEachFeature: function(feature,layer){
         console.log(layer);
         layer.bindPopup("Airport Code:" + feature.properties.faa + "<br> Airport Name:" + feature.properties.name);
     }
 }).addTo(map);
-
-// Loop through the cities array and create one marker for each city.
-    // cityData.forEach(function(city) {
-    //     console.log(city)
-    //     L.circleMarker(city.location, {
-    //         // radius: city.population/100000,
-    //         radius: city.population/200000,
-    //         color: "orange",
-    //         fillColor: "#ffffa1"
-    //     })
-    //     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    //     .addTo(map);
-    // });
-    // L.circleMarker([34.0522, -118.2437],{
-    //     radius: 300,
-    //     color: "black",
-    //     fillColor: '#ffffa1'
-    // }).addTo(map);
-
-    // L.circle([34.0522, -118.2437], {
-    //     radius: 100
-    //  }).addTo(map);
-
-    //  Add a marker to the map for Los Angeles, California.
-    // let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-preview-night-v3/tiles/{z}/{x}/{y}?access_token={accessToken}', {
